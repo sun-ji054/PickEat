5354dd4c90903071b26bd64c2bc1e1ca9b806fa6
sun-ji054 <seonji04203645@gmail.com>
2026-06-08 13:02:38 +0900

    feat: DEBUG = Flase

--- PickEatProject/PickEatProject/settings.py
+++ PickEatProject/PickEatProject/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-in-production'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
